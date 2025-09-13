96c4d82550b009bd3a7ace9dc6a42cdee8e0e2e5
Jose Mario Monge <kukimonge@me.com>
2025-09-12 21:31:22 -0600

    Added process monitoring function and logging in ejercicio2.sh

--- ejercicio2.sh
+++ ejercicio2.sh
@@ -47,5 +47,38 @@ $proceso & 2>/dev/null
 # Guardamos el PID del proceso en curso en una variable local.
 pid=$!
 
-# Configuramos un archivo de log con encabezados para registrar el consumo de CPU y memoria del proceso que se va a monitorear.
-echo "TIEMPO  %CPU  %MEMORIA "
+# Verificamos si el proceso se inició correctamente tras dar tiempo para que se inicialice.
+# NOTA: Usamos 2>/dev/null para evitar que se muestre un mensaje de error adicional si el proceso no se pudo iniciar.
+sleep 1
+if ! kill -0 "$pid" 2>/dev/null; then
+    echo ""
+    echo "ERROR: El proceso '$proceso' no se pudo ejecutar."
+    help
+    exit 1
+fi
+
+# Configuramos el nombre del archivo de log, incluyendo la fecha actual.
+log="log_$(date +%Y-%m-%d)_$procesoPrincipal.txt"
+
+# Configuramos el archivo log para registrar el consumo de CPU y memoria mientras ejecutamos el proceso.
+# NOTA: Usamos 'tee' para mostrar la información en pantalla y guardarla en el archivo de log al mismo tiempo.
+echo ""
+echo "CONSUMO DE CPU Y MEMORIA DEL PROCESO '$proceso' (PID: $pid):" | tee "$log"
+echo "" | tee -a "$log"
+echo "FECHA Y HORA        | %CPU | %MEM |" | tee -a "$log"
+echo "-----------------------------------" | tee -a "$log"
+
+# Usamos un 'while' loop para monitorear el consumo de CPU y memoria del proceso cada 10 segundos.
+# NOTA: Usamos 'kill -0' para verificar si el proceso sigue en ejecución.
+while kill -0 "$pid" 2>/dev/null; do
+    # Extraemos el consumo de CPU y memoria del proceso usando 'ps' y 'awk'.
+    consumo=$(ps -p "$pid" -o %cpu,%mem | tail -1)
+    consumoCPU=$(echo $consumo | awk '{print $1}')
+    consumoMEM=$(echo $consumo | awk '{print $2}')
+
+    # Registramos la fecha y hora actuales junto con el consumo de CPU y memoria en elarchivo de log.
+    echo "$(date +%Y-%m-%d\ %H:%M:%S) | $consumoCPU  | $consumoMEM  |" | tee -a "$log"
+
+    # Esperamos 10 segundos antes de la siguiente iteración.
+    sleep 10
+done
