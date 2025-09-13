e1b506d9fc516f01be4294118ca7563380bad36f
Jose Mario Monge <kukimonge@me.com>
2025-09-13 10:26:21 -0600

    Implemented event logging and improved error handling in ejercicio3.sh

--- ejercicio3.sh
+++ ejercicio3.sh
@@ -3,14 +3,21 @@
 # EJERCICIO 3 - Scripting y servicios.
 # Este script monitorea continuamente un directorio en busca de eventos de creación, modificación y eliminación.
 
-# Verificamos que el comando 'inotifywait' está instalado en el sistema.
+# Configuramos una variable local para el archivo log donde se van a registrar los eventos monitoreados.
+log="log_monitoreo_home.txt"
+
+# Detenemos el script y mostramos un mensaje de error en el archivo log si el comando 'inotifywait' no está instalado en el sistema.
 if [[ -z $(command -v inotifywait 2>/dev/null) ]]; then
-    echo ""
-    echo "ERROR: No se pudo iniciar el monitoreo. Verifique que inotify-tools está instalado."
-    echo ""
+    echo "ERROR: No se pudo iniciar el monitoreo. Verifique que inotify-tools está instalado." > "$log"
     exit 1
 fi
 
-# Configuramos una variable local para el directorio a monitorear y para el archivo log donde se van a registrar los eventos monitoreados.
-directorio="/home"
-log="log_monitoreo_home.txt"
+# Configuramos el archivo log para registrar los eventos monitoreados.
+echo "MONITOREO DEL DIRECTORIO /home - $(date +%d/%m/%Y)" > "$log"
+echo "" >> "$log"
+echo "FECHA Y HORA          | ARCHIVO                      |  EVENTO  " >> "$log"
+echo "----------------------+------------------------------+----------" >> "$log"
+
+# Iniciamos el monitoreo del directorio especificado, registrando los eventos en el archivo log.
+# NOTA: Usamos '--exclude' para evitar un bucle infinito al monitorear, específicamente en caso de que el script se ejecute desde /home y el archivo log se cree ahí.
+inotifywait -mre create,modify,delete /home --exclude 'log_monitoreo_home\.txt' --timefmt '%d/%m/%Y %H:%M:%S' --format '%T    %w%f  %e' 2>/dev/null >> "$log"
