78b115818fe7ad24ff0d8fc98d0f123d3144d05d
Jose Mario Monge <kukimonge@me.com>
2025-09-12 21:24:57 -0600

    Added help function, fixed minor bugs in ejercicio2.sh

--- ejercicio2.sh
+++ ejercicio2.sh
@@ -3,13 +3,20 @@
 # EJERCICIO 2 - Scripting y procesos.
 # Este script permite monitorear el consumo de CPU y memoria de un proceso específico.
 
+# Definimos una función de ayuda para mostrar la guía de uso del script.
+help() {
+    echo ""
+    echo "Este script permite monitorear el consumo de CPU y memoria de un proceso específico."
+    echo ""
+    echo "Uso: $0 [nombre_proceso]"
+    echo ""
+}
+
 # Mostramos un mensaje de error si no se le pasan argumentos al script.
 if [ "$#" -eq 0 ]; then
     echo ""
     echo "ERROR: Debe especificar el nombre de un proceso para monitorear."
-    echo ""
-    echo "Uso: $0 [nombre_proceso]"
-    echo ""
+    help
     exit 1
 fi
 
@@ -21,12 +28,15 @@ proceso="$*"
 # NOTA: Usamos la expresión regular para detectar si el proceso se ejecuta con 'sudo' y extraemos el nombre correcto.
 if [[ "$proceso" =~ ^sudo[[:space:]] ]]; then
     procesoPrincipal=$(basename "$(echo "$*" | awk '{print $2}')")
+
     # Si el usuario usa 'sudo' pero no especifica ningún proceso, mostramos un mensaje de error y detenemos el script (a través de un código de salida).
-    if [[ -z $procesoPrincipal ]]; then
+    if [[ -z "$procesoPrincipal" ]]; then
         echo ""
-        echo "ERROR: Debe especificar el nombre de un proceso para monitorear."
+        echo "ERROR: Debe especificar el nombre de un proceso para monitorear después de 'sudo'."
+        help
         exit 1
     fi
+
 else
     procesoPrincipal=$(basename "$(echo "$*" | awk '{print $1}')")
 fi
