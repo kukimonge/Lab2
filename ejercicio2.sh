10e0e1605f10da5e15f53e984974686a674bc1e5
Jose Mario Monge <kukimonge@me.com>
2025-09-12 19:10:33 -0600

    Added ejercicio2.sh

---
+++ ejercicio2.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# EJERCICIO 2 - Scripting y procesos.
+# Este script permite monitorear el consumo de CPU y memoria de un proceso específico.
+
+# Mostramos un mensaje de error si no se le pasan argumentos al script.
+if [ "$#" -eq 0 ]; then
+    echo ""
+    echo "ERROR: Debe especificar el nombre de un proceso para monitorear."
+    echo ""
+    echo "Uso: $0 [nombre_proceso]"
+    echo ""
+    exit 1
+fi
+
+# Configuramos variables local para el proceso especificado por el usuario y el archivo de log donde vamos a registrar los detalles.
+# NOTA: Usamos "$*" en lugar de "$1" u otras estructuras para permitir la ejecución de procesos más complejos o con opciones.
+proceso="$*"
+
+# Extraemos el nombre del proceso principal, para usarlo en el nombre del archivo de log.
+# NOTA: Usamos la expresión regular para detectar si el proceso se ejecuta con 'sudo' y extraemos el nombre correcto.
+if [[ "$proceso" =~ ^sudo[[:space:]] ]]; then
+    procesoPrincipal=$(basename "$(echo "$*" | awk '{print $2}')")
+    # Si el usuario usa 'sudo' pero no especifica ningún proceso, mostramos un mensaje de error y detenemos el script (a través de un código de salida).
+    if [[ -z $procesoPrincipal ]]; then
+        echo ""
+        echo "ERROR: Debe especificar el nombre de un proceso para monitorear."
+        exit 1
+    fi
+else
+    procesoPrincipal=$(basename "$(echo "$*" | awk '{print $1}')")
+fi
+
+# Configuramos el nombre del archivo de log, incluyendo la fecha y hora actuales.
+log="log_$(date +%Y-%m-%d_%H-%M-%S)_$proceso.txt"
+
+# Ejecutamos el proceso indicado por el usuario, en segundo plano.
+$proceso &
+# Guardamos el PID del proceso en una variable local.
+pid=$!
+
+# Configuramos un archivo de log con encabezados para registrar el consumo de CPU y memoria del proceso que se va a monitorear.
+echo "TIEMPO  %CPU  %MEMORIA "
